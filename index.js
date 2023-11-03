9466c9b71692e1768d61f853e9516a48535ec77a
m3ghnaa <meghna.chandrababu@gmail.com>
2023-11-03 18:52:58 +0530

    create 16by16 grid

---
+++ index.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('grid-container');
+    for (let i = 0; i < 16; i++) {
+      for (let j = 0; j < 16; j++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        container.appendChild(gridSquare);
+      }
+    }
