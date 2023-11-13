43da01f6a4566537b48edbe093357826cd6a40d6
m3ghnaa <meghna.chandrababu@gmail.com>
2023-11-13 16:21:21 +0530

    add color picker and style ui

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 
 const container = document.getElementById('grid-container');
-let isDrawing = false;
+
 
 function createGrid() {
-  for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
+  const gridSizeInput = document.getElementById('grid-size');
+  const gridSize = parseInt(gridSizeInput.value, 10);
+
+  if(isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert('Please enter a valid grid size between 1 and 100');
+    return;
+  }
+
+  const colorPicker = document.getElementById('color-picker');
+  selectedColor = colorPicker.value;
+
+  const selectedColorBox = document.getElementById('selected-color');
+  selectedColorBox.style.backgroundColor = selectedColor;
+
+  container.innerHTML = '';
+
+  const squareSize = container.clientWidth / gridSize;
+
+  for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
       const gridSquare = document.createElement('div');
       gridSquare.classList.add('grid-square');
+      gridSquare.style.width = `${squareSize}px`;
+      gridSquare.style.height = `${squareSize}px`;
       container.appendChild(gridSquare);
 
       gridSquare.addEventListener('mousedown', () => {
         isDrawing = true;
-        gridSquare.style.backgroundColor = '#FF5733';
+        gridSquare.style.backgroundColor = selectedColor;
       });
 
       gridSquare.addEventListener('mousemove', () => {
         if (isDrawing) {
-          gridSquare.style.backgroundColor = '#FF5733';
+          gridSquare.style.backgroundColor = selectedColor;
         }
       });
 
@@ -25,6 +45,8 @@ function createGrid() {
       });
     }
   }
+  gridSizeInput.value = gridSize;
 }
+
 
 createGrid();
