3b4c36e530b5ef43290357fe484821546633e808
m3ghnaa <meghna.chandrababu@gmail.com>
2023-11-13 15:18:55 +0530

    add eventlisteners for mouse movements

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+
 const container = document.getElementById('grid-container');
-    for (let i = 0; i < 16; i++) {
-      for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        container.appendChild(gridSquare);
-      }
+let isDrawing = false;
+
+function createGrid() {
+  for(let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j++) {
+      const gridSquare = document.createElement('div');
+      gridSquare.classList.add('grid-square');
+      container.appendChild(gridSquare);
+
+      gridSquare.addEventListener('mousedown', () => {
+        isDrawing = true;
+        gridSquare.style.backgroundColor = '#FF5733';
+      });
+
+      gridSquare.addEventListener('mousemove', () => {
+        if (isDrawing) {
+          gridSquare.style.backgroundColor = '#FF5733';
+        }
+      });
+
+      gridSquare.addEventListener('mouseup', () => {
+        isDrawing = false;
+      });
     }
+  }
+}
+
+createGrid();
