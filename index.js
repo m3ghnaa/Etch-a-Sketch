4d70b11e311ff309dc22f08853e15753f8c90e14
m3ghnaa <meghna.chandrababu@gmail.com>
2023-11-14 00:45:08 +0530

    add eraser, clear btn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
+
 const container = document.getElementById('grid-container');
+let isDrawing = false;
+let isErasing = false;
+let selectedColor = '#FF5733';
+
 
 
 function createGrid() {
@@ -29,9 +34,12 @@ function createGrid() {
       gridSquare.style.height = `${squareSize}px`;
       container.appendChild(gridSquare);
 
+      //Mouse events
       gridSquare.addEventListener('mousedown', () => {
-        isDrawing = true;
-        gridSquare.style.backgroundColor = selectedColor;
+        if (!isErasing) {
+          isDrawing = true;
+          gridSquare.style.backgroundColor = selectedColor;
+        }
       });
 
       gridSquare.addEventListener('mousemove', () => {
@@ -43,10 +51,73 @@ function createGrid() {
       gridSquare.addEventListener('mouseup', () => {
         isDrawing = false;
       });
+
+      //Touch events
+      gridSquare.addEventListener('touchstart', (event) => {
+        isDrawing=true;
+        handleTouch(event);
+      });
+
+      gridSquare.addEventListener('touchmove', (event) => {
+        if(isDrawing){
+          handleTouch(event);
+        }
+      });
+
+      gridSquare.addEventListener('touchend', (event) => {
+        isDrawing = false;
+      });
+
+      
     }
   }
   gridSizeInput.value = gridSize;
 }
+
+
+
+const colorPicker = document.getElementById('color-picker');
+colorPicker.addEventListener('input', () => {
+  selectedColor =  colorPicker.value;
+
+  const selectedColorBox = document.getElementById('selected-color');
+  selectedColorBox.style.backgroundColor = selectedColor;
+})
+
+function handleTouch(event, gridSquare) {
+  event.preventDefault();
+
+  const touch = event.touches[0];
+  const target = document.elementFromPoint(touch.clientX, touch.clientY);
+
+  if (target && target.classList.contains('grid-square')) {
+    target.style.backgroundColor = selectedColor;
+  }
+}
+
+let selectedColorBox = document.getElementById('selected-color');
+
+function toggleEraser() {
+  console.log('Toggling eraser');
+
+  // Use a tertiary operator to toggle between drawing color and eraser color
+  selectedColor = selectedColor === 'white' ? '#FF5733' : 'white';
+
+  console.log(selectedColor);
+
+  // Update the selectedColorBox and color picker
+  selectedColorBox.style.backgroundColor = selectedColor;
+ 
+}
+
+function clearGrid() {
+  const gridSquares = document.querySelectorAll('.grid-square');
+  gridSquares.forEach((gridSquare) => {
+    gridSquare.style.backgroundColor = 'white';
+  });
+}
+
+
 
 
 createGrid();
